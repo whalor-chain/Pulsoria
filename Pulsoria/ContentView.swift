64624ed0d14d2076ad6c440482f5e61adc0589d3
Whalor <stasagunker@gmailcom>
2026-04-06 19:43:48 +0300

    Initial Commit

---
+++ Pulsoria/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pulsoria
+//
+//  Created by Станислава Гункер on 06.04.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pulsoria/PulsoriaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulsoriaApp.swift
+//  Pulsoria
+//
+//  Created by Станислава Гункер on 06.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct PulsoriaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
